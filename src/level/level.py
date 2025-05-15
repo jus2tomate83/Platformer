b1f4df7a0158033c8da2cda1c24478941e19dcbf
Lefebvre Benjamin <121179070+jus2tomate83@users.noreply.github.com>
2025-05-15 19:23:38 +0200

    add platforms + platforms colliding

--- src/level/level.py
+++ src/level/level.py
@@ -1,5 +1,5 @@
 from src.settings import *
-from src.sprite.sprite import Sprite
+from src.sprite.sprite import Sprite, MovingSprite
 from src.player.player import Player
 
 class Level:
@@ -13,15 +13,34 @@ class Level:
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
+        #tiles
         for x,y,surf in  tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILED_SIZE,y * TILED_SIZE),surf, (self.all_sprites,self.collision_sprites) )
 
+        #objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
                 Player((obj.x, obj.y), (self.all_sprites,), self.collision_sprites)
 
+        #moving object
+        for obj in tmx_map.get_layer_by_name('Moving Objects'):
+            if obj.name == 'helicopter':
+                if obj.width > obj.height:
+                    #horizontal
+                    move_dir = 'x'
+                    start_pos = (obj.x, obj.y + obj.height / 2)
+                    end_pos = (obj.x + obj.width, obj.height / 2)
+                else:
+                    #vertical
+                    move_dir = 'y'
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width / 2, obj.height)
+                speed = obj.properties['speed']
+                MovingSprite((self.all_sprites, self.collision_sprites), start_pos, end_pos, move_dir, speed)
+
+
     def run(self, dt):
-        self.all_sprites.update(dt)
         self.display_surface.fill('black')
+        self.all_sprites.update(dt)
         self.all_sprites.draw(self.display_surface)
 
